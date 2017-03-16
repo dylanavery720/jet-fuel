dca9cc43b691d89054286f005b9a0f656873a4c1
Noah Peden <noahpeden@gmail.com>
2017-03-16 17:42:08 -0600

    four passing server tests

--- server.js
+++ server.js
@@ -82,6 +82,10 @@ app.post('/api/urls/:name', (request, response) => {
   })
 })
 
-app.listen(app.get('port'), () => {
-  console.log(`${app.locals.title} is running on ${app.get('port')}.`)
-})
+if (!module.parent) {
+  app.listen(app.get('port'), () => {
+    console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+  });
+}
+
+module.exports = app;
--- tests/server-test.js
+++ tests/server-test.js
@@ -1,3 +1,7 @@
+process.env.NODE_ENV = 'test';
+
+const configuration = require('../knexfile')['test'];
+const database = require('knex')(configuration);
 const chai = require('chai');
 const expect = chai.expect;
 const chaiHttp = require('chai-http');
@@ -6,6 +10,18 @@ const app = require('../server.js');
 chai.use(chaiHttp);
 
 describe('Server', () => {
+  beforeEach(function(done) {
+  database.migrate.rollback()
+  .then(function() {
+    database.migrate.latest()
+    .then(function() {
+      return database.seed.run()
+      .then(function() {
+        done();
+      });
+    });
+  });
+});
   it('should exist', () => {
     expect(app).to.exist;
   });
@@ -23,105 +39,49 @@ describe('Server', () => {
     });
   });
 
-  describe('GET /api/v1/pizzas', function() {
-    beforeEach(function(done){
-      const pizzas = [{type: 'cheese'},
-                      {type: 'meat'},
-                      {type: 'pineapple'},
-                      {type: 'sardine'}];
-      app.locals.pizzas = pizzas;
-      done();
-    });
-
-    afterEach(function(done){
-      app.locals.pizzas = [];
-      done();
-    });
-
-    it('should return all pizzas', function(done) {
-      chai.request(app)
-      .get('/api/v1/pizzas')
-      .end(function(err, res) {
-        if (err) { done(err); }
-        expect(res).to.have.status(200);
-        expect(res).to.be.json;
-        expect(res.body).to.be.a('array');
-        expect(res.body.length).to.equal(4);
-        expect(res.body[0]).to.have.property('type');
-        done();
-      });
-    });
-  });
-
-  describe('GET /api/v1/pizza/:id', function() {
-    beforeEach(function(done){
-      const pizzas = [{type: 'vodka', id: 1},
-                      {type: 'sausage', id: 2}];
-      app.locals.pizzas = pizzas;
-      done();
-    });
-
-    afterEach(function(done){
-      app.locals.pizzas = [];
-      done();
-    });
-
-    context('if pizza is found', function(){
-      it('should return a specific pizza', function(done) {
+    describe('GET /api/folders', function() {
+      it('should return all folders', function(done) {
         chai.request(app)
-        .get('/api/v1/pizza/2')
+        .get('/api/folders')
         .end(function(err, res) {
           if (err) { done(err); }
           expect(res).to.have.status(200);
           expect(res).to.be.json;
-          expect(res.body).to.be.a('object');
-          expect(res.body).to.have.property('type');
-          expect(res.body.type).to.equal('sausage');
+          expect(res.body).to.be.a('array');
+          expect(res.body.length).to.equal(2);
+          expect(res.body[0]).to.have.property('name');
           done();
         });
       });
     });
 
-    context('if no pizza is found', function(){
-      it('should return a 404', function(done) {
-        chai.request(app)
-        .get('/api/v1/pizza/3')
-        .end(function(err, res) {
-          expect(res).to.have.status(404);
-          expect(res.body).to.be.a('object');
-          done();
+    describe('GET /api/folders/:id', function() {
+      context('if folder is found', function(){
+        it('should return a specific folder', function(done) {
+          chai.request(app)
+          .get('/api/folders/2')
+          .end(function(err, res) {
+            if (err) { done(err); }
+            expect(res).to.have.status(200);
+            expect(res).to.be.json;
+            expect(res.body).to.be.a('array');
+            expect(res.body).to.have.property('name');
+            expect(res.body.type).to.equal('Bob Barker');
+            done();
+          });
+        });
+      });
+
+      context('if no folder is found', function(){
+        it('should return a 404', function(done) {
+          chai.request(app)
+          .get('/api/v1/pizza/3')
+          .end(function(err, res) {
+            expect(res).to.have.status(404);
+            expect(res.body).to.be.a('object');
+            done();
+          });
         });
       });
     });
   });
-
-  describe('POST /api/v1/pizzas', function() {
-    afterEach(function(done){
-      app.locals.pizzas = [];
-      done();
-    });
-
-    context('if pizza is valid', function(){
-      it('should return a specific pizza', function(done) {
-        chai.request(app)
-        .post('/api/v1/pizzas')
-        .send({
-          type: 'cheese'
-        })
-        .end(function(err, res) {
-          if (err) { done(err); }
-          expect(res).to.have.status(200);
-          expect(res).to.be.json;
-          expect(res.body).to.be.a('object');
-          expect(res.body).to.have.property('type');
-          expect(res.body.type).to.equal('cheese');
-          done();
-        });
-      });
-    });
-
-    context('if pizza is not valid', function(){
-      it('should return a 422 Unprocessable Entity status code');
-    });
-  });
-});
